be5675c4b441570943f2b1c23767885e923b2997
selim yalinkilic <selimyalinkilic@selims-MacBook-Pro.local>
2021-02-22 23:35:29 +0300

    added new url type

--- src/components/Articles/Card/index.jsx
+++ src/components/Articles/Card/index.jsx
@@ -3,11 +3,23 @@ import { Link } from "react-router-dom";
 import { Col } from "react-bootstrap";
 import "./Card.css";
 
-const convertToSlug = (text) => {
-  return text
-    .toLowerCase()
-    .replace(/ /g, "-")
-    .replace(/[^\w-]+/g, "");
+const convertToSlug = (str) => {
+  str = str.replace(/^\s+|\s+$/g, ""); // trim
+  str = str.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+  var to = "aaaaaeeeeeiiiiooooouuuunc------";
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+  }
+
+  str = str
+    .replace(/[^a-z0-9 -]/g, "") // remove invalid chars
+    .replace(/\s+/g, "-") // collapse whitespace and replace by -
+    .replace(/-+/g, "-"); // collapse dashes
+
+  return str;
 };
 
 const Card = ({ article, w }) => {

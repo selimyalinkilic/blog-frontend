27b8208de6ac0f83f805e97e124d9894f65178ea
selim yalinkilic <selimyalinkilic@selims-MacBook-Pro.local>
2021-03-08 23:47:08 +0300

    updated twitter meta tags

--- src/components/Seo/ArticleSeo.jsx
+++ src/components/Seo/ArticleSeo.jsx
@@ -8,7 +8,8 @@ const ArticleSeo = ({ article }) => {
       <meta name="twitter:site" content="@Yagizdoo" />
       <meta name="twitter:title" content={article?.title} />
       <meta name="twitter:description" content={article?.summary} />
-      <meta name="twitter:image" content={article?.thumbnail} />
+      <meta name="twitter:creator" content="@Yagizdoo" />
+      <meta name="twitter:image:src" content={article?.thumbnail} />
       <meta property="og:url" content={window.location.href} />
       <meta property="og:title" content={article?.title} />
       <meta property="og:description" content={article?.summary} />
